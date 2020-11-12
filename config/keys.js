ca4f9e2388df1f3b8c835559dc805ff8b5fe89c3
Ashwin <ashwin.dinesh01@gmail.com>
2020-11-12 10:37:15 +0530

    Add keys file

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: '<client-id>',
+        clientSecret: '<secret>'
+    },
+    mongodb: {
+        dbURI: 'mongodb://root:pass123@localhost:27017/google_user_db?authSource=admin'
+    },
+    session: {
+        cookieKey: 'thisisasecret'
+    }
+};
